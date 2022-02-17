8ba9cb1155b16aa918dd02b111d35fb7ccf5d872
Thi-Ponce <thi.pponce@gmail.com>
2022-02-17 19:35:05 -0300

    Clear completed

--- modules/modify.js
+++
@@ -1,9 +0,0 @@
-export default (items) => {
-  const descrips = document.querySelectorAll('.itemDescrip');
-  descrips.forEach((task, index) => {
-    task.addEventListener('input', () => {
-      items[index].description = task.value;
-      localStorage.localData = JSON.stringify(items);
-    });
-  });
-};
