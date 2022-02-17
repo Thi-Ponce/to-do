8dfa2e995327f45543728ebf3bf0f552528bde4c
Thi-Ponce <thi.pponce@gmail.com>
2022-02-17 19:15:15 -0300

    Fix errors

---
+++ modules/completed.js
@@ -0,0 +1,13 @@
+const checkBox = (index, items) => {
+  items[index].completed = !items[index].completed;
+  localStorage.localData = JSON.stringify(items);
+};
+
+export default (items) => {
+  const checkboxes = document.querySelectorAll('.checkbox');
+  checkboxes.forEach((checkbox, index) => {
+    checkbox.addEventListener('change', () => {
+      checkBox(index, items);
+    });
+  });
+};
